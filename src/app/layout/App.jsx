e043053f55a1dec995d1d30ca85eb98371f1023c
FanonX Rogers <fanonxdevelops@gmail.com>
2019-07-04 22:46:09 -0400

    Project setup: initial commit

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class App extends Component {
+    render() {
+        return (
+            <div>Hellno</div>
+        )
+    }
+}
+
+export default App;
